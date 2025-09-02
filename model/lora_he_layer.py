23f4cd5a75912e81ef1208852b366481a162b068
Sujeong567 <kimsj20@ewha.ac.kr>
2025-09-02 15:50:42 +0900

    [FEAT] lora layer 및 [REFACTOR] 일부 데이터 파일 구조 변경

--- model/lora_he_layer.py
+++ model/lora_he_layer.py
@@ -1,32 +1,16 @@
-#LoRA 동형암호 레이어(연산/클래스)
+# model/lora_he_layer.py
+# model/fhe_ckks_local.py에 정의된 범용 암호화 함수를 임포트합니다.
+from model.fhe_ckks_local import ckks_encrypt # <- Context 관련 함수는 임포트하지 않습니다.
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification 
-import torch
+def encrypt_lora_input(input_vector, encryption_context):
+    """
+    LoRA 입력 벡터를 CKKS 방식으로 암호화하는 함수.
+    model/fhe_ckks_local.py의 ckks_encrypt 함수를 내부적으로 호출합니다.
+    encryption_context를 인자로 받아서 ckks_encrypt에 전달합니다.
+    """
+    # encryption_context가 ckks_encrypt 함수의 Context 인자와 동일한 역할을 합니다.
+    encrypted_vec = ckks_encrypt(input_vector, encryption_context)
+    return encrypted_vec
 
-class LoRAHELayer: 
-    pass
-
-model_name = "prajjwal1/bert-tiny"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-model.to("cpu")  # GPU 없으면 "cpu" 사용
-
-def load_model (model_name, device):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-    return tokenizer, model.to(device)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-input = input.to(device)
-
-from generate_dummy_encrypted import load_data
-
-texts, labels = load_data()
-
-dataset = MyDataset(texts, labels, tokenizer, max_length=512)
-#dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
+# LoRA 가중치 적용, 추론 로직 등 이곳에 정의될 수 있습니다.
+# (예: def apply_he_lora(encrypted_input, encrypted_lora_weights, encryption_context): ...)
